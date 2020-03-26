203792417429badc96cee0fc52c5700653ce5685
Jomoflash <jomoflash22@gmail.com>
2020-03-26 07:34:43 +0100

    first commit, assignment is ready.

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
+/*
+*	Hello !
+*	This is a javascript application for startNg task 1
+*/
 
-console.log("Hello World");
+var myName = "Emmanuel James";
+
+var courses = ["front-end", "back-end", "mobile", "coding",];
+
+console.log(myName + "\n");
+console.log("I'm offering " + courses.length + " courses which are :") ;
+	
+for (var i = 0 ; i < courses.length; i++)
+	{
+		console.log(i+1 +": " + courses[i]);
+	}
+console.log("\n");
+// This part diplays all even numbers from 1 to 200
+for (var i = 0; i <= 200; i+=2)
+	{
+		console.log(i);
+	}
+
+
+
+
